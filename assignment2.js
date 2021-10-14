4bedf8e04ee09602f3665733811628249ee97ab1
edanchu <91294716+edanchu@users.noreply.github.com>
2021-10-14 09:51:16 -0700

    Initial commit

---
+++ assignment2.js
@@ -0,0 +1,204 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Shader, Graphics_Card_Object, Texture
+} = tiny;
+
+class Cube extends Shape {
+    constructor() {
+        super("position", "normal",);
+        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
+
+class Cube_Outline extends Shape {
+    constructor() {
+        super("position", "color");
+        //  TODO (Requirement 5).
+        // When a set of lines is used in graphics, you should think of the list entries as
+        // broken down into pairs; each pair of vertices will be drawn as a line segment.
+        // Note: since the outline is rendered with Basic_shader, you need to redefine the position and color of each vertex
+
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1],  [-1, -1,-1], [-1, 1,-1], [-1, -1,-1], [-1, -1, 1], [-1, 1, -1], [1, 1, -1],
+            [1, -1, -1], [1, 1, -1],  [-1, 1,-1], [-1, 1, 1], [1, 1,-1], [1, 1, 1], [1, -1, -1], [1, -1, 1],
+            [-1, -1, 1], [-1, 1, 1],  [-1, -1, 1], [1, -1,1], [-1, 1,1], [1, 1, 1], [1, 1, 1], [1, -1, 1]);
+
+        this.arrays.color.push(color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1),
+            color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1),
+            color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1),
+            color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1), color(1,1,1,1));
+
+        this.indices = false;
+
+    }
+}
+
+class Cube_Single_Strip extends Shape {
+    constructor() {
+        super("position", "normal");
+
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, 1, -1], [1, 1, -1], [-1, -1, 1], [1, -1, 1], [-1, 1, 1],
+            [1, 1, 1]);
+
+        this.arrays.normal = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, 1, -1], [1, 1, -1], [-1, -1, 1], [1, -1, 1], [-1, 1, 1],
+            [1, 1, 1]);
+
+        this.indices.push(7,6,5,4,0,5,1,7,3,6,2,4,0,2,1,3);
+
+    }
+}
+
+class Triangle_Strip_Plane extends Shape{
+    constructor(length, width, origin, density){
+        super("position", "normal", "texture_coord");
+        let denseWidth = width * density;
+        let denseLength = length * density;
+        for (let z = 0; z < denseWidth; z++){
+            for (let x = 0; x < denseLength; x++){
+                this.arrays.position.push(Vector3.create(x/density - length/2 + origin[0] + 1,origin[1],z/density - width/2 + origin[2] + 1));
+                this.arrays.texture_coord.push(Vector.create(x/denseLength,1 - (z/denseWidth)));
+            }
+        }
+        this.arrays.normal.push.apply(this.arrays.normal, this.arrays.position);
+
+        let indices = [];
+
+        for (let z = 0; z < denseWidth - 1; z++) {
+            if (z > 0) this.indices.push(z * denseLength);
+            for (let x = 0; x < denseLength; x++) {
+                indices.push((z * denseLength) + x, ((z + 1) * denseLength) + x);
+            }
+            if (z < denseWidth - 2) this.indices.push(((z + 2) * denseLength) - 1);
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--){
+            this.indices.push(indices[i]);
+        }
+    }
+}
+
+class Offset_shader extends Shader {
+    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
+        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
+            PCM = P.times(C).times(M);
+        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false, Matrix.flatten_2D_to_1D(PCM.transposed()));
+        context.uniform4fv(gpu_addresses.color, material.color);
+
+        context.uniform1i(gpu_addresses.texture, 0);
+        material.texture.activate(context);
+    }
+
+    shared_glsl_code() {
+        return `precision mediump float;
+                uniform sampler2D texture;
+                varying vec2 f_tex_coord;   
+            `;
+    }
+
+    vertex_glsl_code() {
+        return this.shared_glsl_code() + `
+                attribute vec3 position;                         
+                uniform mat4 projection_camera_model_transform;
+                attribute vec2 texture_coord; 
+                
+                void main(){
+                    vec4 tex_color = texture2D(texture, texture_coord);
+                    gl_Position = projection_camera_model_transform * vec4( position.x, position.y - tex_color.y, position.z, 1.0 );
+                    f_tex_coord = texture_coord;
+                }`;
+    }
+
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+                uniform vec4 color;
+                void main(){
+                    vec4 tex_color = texture2D(texture, f_tex_coord);
+                    gl_FragColor = tex_color;
+                }`;
+    }
+}
+
+class Base_Scene extends Scene {
+    constructor() {
+        super();
+
+        this.offsets = [];
+        for (let z = 0; z < 5; z++){
+            for (let x = 0; x < 5; x++){
+                this.offsets.push(Vector3.create(0, z, 0));
+            }
+        }
+
+        this.texture = new Texture("assets/heightmap.png");
+
+        this.shapes = {
+            'cube': new Cube(),
+            'outline': new Cube_Outline(),
+            'single_strip' : new Cube_Single_Strip(),
+            'plane' : new Triangle_Strip_Plane(20,20, Vector3.create(0,0,0), 10),
+            'axis' : new defs.Axis_Arrows()
+        };
+
+        this.materials = {
+            plastic: new Material(new defs.Phong_Shader(),
+                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+            offset: new Material(new Offset_shader(), {color: hex_color("#2b3b86"), texture: this.texture})
+        };
+        this.white = new Material(new defs.Basic_Shader());
+    }
+
+    display(context, program_state) {
+        // display():  Called once per frame of animation. Here, the base class's display only does
+        // some initial setup.
+
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            let initialCameraPos = Mat4.identity();
+            initialCameraPos = initialCameraPos.times(Mat4.translation(0, -5, -20));
+            program_state.set_camera(initialCameraPos);
+        }
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        // *** Lights: *** Values of vector or point lights.
+        const light_position = vec4(0, 5, 5, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+    }
+}
+
+export class Assignment2 extends Base_Scene {
+    constructor() {
+        super();
+    }
+
+
+    make_control_panel() {
+        this.key_triggered_button("Placeholder", ["c"], () => 1);
+    }
+
+
+    display(context, program_state) {
+        super.display(context, program_state);
+
+        let model_transform = Mat4.identity();
+
+        this.shapes.plane.draw(context, program_state, model_transform, this.materials.offset, "TRIANGLE_STRIP");
+        this.shapes.axis.draw(context, program_state, model_transform, this.materials.plastic);
+    }
+}
